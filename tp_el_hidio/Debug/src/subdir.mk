037fa0ece09e05827ded8c6f9dbb16336791163d
u <u@u-VirtualBox.(none)>
2018-06-04 12:17:15 +0900

    remove my library project. hidapi 0.7.0 so build complete

--- tp_el_hidio/Debug/src/subdir.mk
+++ tp_el_hidio/Debug/src/subdir.mk
@@ -4,12 +4,15 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
+../src/hid.c \
 ../src/tp_el_hidio.c 
 
 OBJS += \
+./src/hid.o \
 ./src/tp_el_hidio.o 
 
 C_DEPS += \
+./src/hid.d \
 ./src/tp_el_hidio.d 
 
 
@@ -17,7 +20,7 @@ C_DEPS += \
 src/%.o: ../src/%.c
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C Compiler'
-	gcc -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
+	gcc -I"/home/u/workspace/hidapi/inc" -I/usr/include -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
--- tp_el_hidio/Debug/src/tp_el_hidio.d
+++ tp_el_hidio/Debug/src/tp_el_hidio.d
@@ -1,1 +1,4 @@
-src/tp_el_hidio.o: ../src/tp_el_hidio.c
+src/tp_el_hidio.o: ../src/tp_el_hidio.c \
+ /home/u/workspace/hidapi/inc/hidapi.h
+
+/home/u/workspace/hidapi/inc/hidapi.h:
